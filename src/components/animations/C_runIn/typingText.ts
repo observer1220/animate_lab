62965b6cdf457ac481f9164d3ea974492bb9178a
ＫＥＮＴ <kent@Kent-Mac-mini.local>
2025-01-24 17:17:57 +0800

    2025-01-24

--- src/components/animations/C_runIn/typingText.ts
+++ src/components/animations/C_runIn/typingText.ts
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
-import "./typingText.css";
+import "./TypingText.css";
 
 /** 進場動態 */
 const TypingText: React.FC = () => {
