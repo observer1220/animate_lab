e47ff972cbe382709aedb386cb8b9b4bd2f844ca
ＫＥＮＴ <kent@Kent-Mac-mini.local>
2025-01-24 17:30:50 +0800

    2025-01-24

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/music_festival/",
+  base: "/block_design_react/",
 });
